b33daa96aac2ce8b6c2152991e2a4ca8b3b3cd92
Ricardo Morin <morinricardo@gmail.com>
2022-09-04 19:40:18 -0700

    Initial commit

---
+++ analysis/compute_total_population.py
@@ -0,0 +1,28 @@
+layer = iface.activeLayer()
+
+features = layer.getFeatures()
+
+index = QgsSpatialIndex(layer.getFeatures())
+print ("Created spatial index")
+
+counter = 0
+
+
+for polygon in features:
+    polygonId = polygon.id()
+    polygon['POP_TOTAL'] = polygon['POP_EST_19']
+    layer.updateFeature(polygon)
+    pointOnPolygon = polygon.geometry().pointOnSurface()
+    p = pointOnPolygon.asPoint()
+    isect = index.intersects(QgsRectangle(p,p))
+    for i in isect:
+        if polygonId != i:
+            candidateFeature = layer.getFeature(i)
+            if candidateFeature.geometry().intersects(pointOnPolygon):
+                if polygon.geometry().area() < candidateFeature.geometry().area():
+                    polygon['POP_TOTAL'] += candidateFeature['POP_EST_19']
+                    layer.updateFeature(polygon)
+                    counter += 1
+                    print(polygon['Water_prov'], "adds population from", candidateFeature['Water_prov'])
+print(counter, "polygons visited")
+    
